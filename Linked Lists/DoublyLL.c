04a45c5d5d0ac92f9221bf6c57a4eaf3a5bac2f3
swarajmhatre <swarajmhatre25@gmail.com>
2023-08-03 01:52:26 +0530

    Added Doubly Linked List

--- Linked Lists/DoublyLL.c
+++ Linked Lists/DoublyLL.c
@@ -4,39 +4,43 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //
-//________________________________________This is the code which does all the operation on a Singly Linked List__________________________________________
+//________________________________________This is the code which does all the operation on a Doubly Linked List__________________________________________
 //______________________________________________________________Feel free to contribute__________________________________________________________________
 //
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Node Structure
+// Node Structure
 typedef struct Node
 {
     int data;
     struct Node *next;
+    struct Node *prev;
 } NODE, *PNODE, **PPNODE;
+
 
 // Count all nodes in the Linked List
 int Count(PNODE Head)
 {
     int iCnt = 0;
-    while (Head != NULL)
+    PNODE temp = Head;
+
+    while (temp->next != NULL)
     {
         iCnt++;
-        Head = Head->next;
+        temp = temp->next;
     }
     return iCnt;
 }
 
 //Insert a new node at first position
-void InsertFirst(PPNODE Head, int No)
+void InsertFirst(PPNODE Head, int iNo)
 {
     PNODE newn = NULL;
 
     newn = (PNODE)malloc(sizeof(NODE));
-
-    newn->data = No;
+    newn->data = iNo;
+    newn->prev = NULL;
     newn->next = NULL;
 
     if (*Head == NULL)
@@ -45,20 +49,21 @@ void InsertFirst(PPNODE Head, int No)
     }
     else
     {
+        (*Head)->prev = newn;
         newn->next = *Head;
         *Head = newn;
     }
 }
 
-//Insert a new node at last position
-void InsertLast(PPNODE Head, int No)
+//Insert a new node at Last position
+void InsertLast(PPNODE Head, int iNo)
 {
     PNODE newn = NULL;
     PNODE temp = *Head;
-
     newn = (PNODE)malloc(sizeof(NODE));
 
-    newn->data = No;
+    newn->data = iNo;
+    newn->prev = NULL;
     newn->next = NULL;
 
     if (*Head == NULL)
@@ -72,130 +77,125 @@ void InsertLast(PPNODE Head, int No)
             temp = temp->next;
         }
         temp->next = newn;
+        newn->prev = temp;
     }
 }
- 
-//Insert a new node at the desired position
-void InsertAtPos(PPNODE Head, int No, int iPos)
-{
-    int iLength = Count(*Head);
 
-    PNODE newn = (PNODE)malloc(sizeof(NODE));
-    if ((iPos < 1) || (iPos > iLength + 1))
-    {
-        printf("Invalid Input.\n");
+//Insert a new node at the desired position
+void InsertAtPos(PPNODE Head, int iNo, int iPos){
+    int iLength = Count(*Head);
+    PNODE newn = NULL;
+    PNODE temp = *Head;
+
+    if((iPos<1)|| (iPos> iLength+1)){
+        printf("\nInvalid Input.\n");
         return;
     }
-
-    if (iPos == 1)
-    {
-        InsertFirst(Head, No);
+    if(iPos == 1){
+        InsertFirst(Head, iNo);
     }
-    else if (iPos == (iLength + 1))
-    {
-        InsertLast(Head, No);
+    else if(iPos == (iLength+1)){
+        InsertLast(Head, iNo);
     }
-    else
-    {
-        PNODE temp = *Head;
-        newn->data = No;
-        newn->next = NULL;
-
-        for (int i = 1; i < (iPos - 1); i++)
-        {
+    else{
+        for(int i =1; i<iPos-1; i++){
             temp = temp->next;
         }
+
+        newn = (PNODE)malloc(sizeof(NODE));
+
+        newn->data = iNo;
+        newn->next = NULL;
+        newn->prev = NULL;
+
         newn->next = temp->next;
+        temp->next->prev = newn;
         temp->next = newn;
+        newn->prev = temp;
     }
 }
 
-//Delete a node from the last position
+//Delete a node from first position
 void DeleteFirst(PPNODE Head)
 {
     PNODE temp = *Head;
+
     if (*Head == NULL)
     {
-        printf("Nothing to delete\n");
+        printf("Nothing to Delete\n");
         return;
     }
-    else if ((*Head)->next == NULL)
-    {
+    else if(((*Head)->next == NULL)){
         free(*Head);
-        return;
+        *Head = NULL;
     }
     else
     {
         *Head = (*Head)->next;
+        (*Head)->prev = NULL;
         free(temp);
     }
 }
 
-//Delete a node from the first position
-void DeleteLast(PPNODE Head)
-{
+//Delete a node from Last position
+void DeleteLast(PPNODE Head){
     PNODE temp = *Head;
 
     if(*Head == NULL){
-        printf("Nothing to Delete.\n");
+        printf("Nothing to Delete\n");
         return;
     }
-    else if((*Head)->next == NULL ){
+    else if((*Head)->next == NULL){
         free(*Head);
-        return;
-    }   
+        *Head = NULL;
+    }
     else{
-        while (temp->next->next != NULL)
+        while ((temp->next->next) != NULL)
         {
             temp = temp->next;
         }
         free(temp->next);
         temp->next = NULL;
     }
-    
 }
 
-//Delete a node from the desired position
-void DeleteAtPos(PPNODE Head, int iPos)
-{
+//Delete a node at the desired position
+void DeleteAtPos(PPNODE Head, int iPos){
     int iLength = Count(*Head);
     PNODE temp = *Head;
     PNODE tempX = NULL;
 
-    if ((iPos < 1) || (iPos > iLength))
-    {
+    if((iPos<1)|| (iPos>iLength)){
         printf("Invalid Input\n");
         return;
     }
 
     if (iPos == 1)
     {
-        DeleteFirst(Head);
+       DeleteFirst(Head);
     }
-    else if (iPos == iLength)
-    {
+    else if(iPos == iLength){
         DeleteLast(Head);
     }
-    else
-    {
-        for (int i = 1; i < iPos - 1; i++)
-        {
+    else{
+         for(int i=1; i< iPos-1; i++){
             temp = temp->next;
-        }
-        tempX = temp->next;
+         }
         temp->next = temp->next->next;
-        free(tempX);
+        free(temp->next->prev);
+        temp->next->prev = temp;
     }
 }
 
-//Display the Linked Lists
+//Display the entire Linked List
 void Display(PNODE Head)
 {
-    printf("Elements of Linked List are: \n");
-    while (Head != NULL)
+    PNODE temp = Head;
+    printf("The elements in the Linked List are: ");
+    while (temp != NULL)
     {
-        printf("|%d| -> ", Head->data);
-        Head = Head->next;
+        printf("|%d|-> ", temp->data);
+        temp = temp->next;
     }
     printf("NULL\n");
 }
@@ -203,29 +203,35 @@ void Display(PNODE Head)
 int main()
 {
     PNODE First = NULL;
-    int iRet = 0;
 
-    InsertFirst(&First, 111);
-    InsertFirst(&First, 101);
     InsertFirst(&First, 51);
     InsertFirst(&First, 21);
     InsertFirst(&First, 11);
+    Display(First);
+    printf("Number of elements: %d \n", Count(First));
 
     InsertLast(&First, 121);
     InsertLast(&First, 151);
+    Display(First);
+    printf("Number of elements: %d \n", Count(First));
+
+
+    InsertAtPos(&First, 221,4);
 
     Display(First);
+    printf("Number of elements: %d \n", Count(First));
+
+    DeleteAtPos(&First, 4);
+    Display(First);
+    printf("Number of elements: %d \n", Count(First));
 
     DeleteFirst(&First);
     Display(First);
+    printf("Number of elements: %d \n", Count(First));
 
     DeleteLast(&First);
     Display(First);
-
-    DeleteAtPos(&First, 2);
-    Display(First);
-
-    printf("Count is: %d", Count(First));
+    printf("Number of elements: %d \n", Count(First));
 
     return 0;
 }
